e8e0b9036c30834d27b8a5a0741221c47247fc74
Tran Xuan Thanh <thanhthcsab@gmail.com>
2023-11-27 20:01:34 +0700

    fix image display error

--- plms/urls.py
+++ plms/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +27,4 @@ urlpatterns = [
     path('clubs/', include('apps.clubs.urls')),
     path('managers/', include('apps.managers.urls')),
     path('more/', include('apps.more.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
