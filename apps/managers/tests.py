aec36e96a499db74ef2b0d329bd28ac25c0af8d6
Mai Anh Quân <108073174+qvan7um@users.noreply.github.com>
2023-12-04 17:50:23 +0700

    add unit test cases for the managers/views.py

--- apps/managers/tests.py
+++ apps/managers/tests.py
@@ -1,10 +1,13 @@
 import os
 import shutil
-from django.conf import settings
 
+from django.conf import settings
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 from django.core.files.uploadedfile import SimpleUploadedFile
-from django.test import RequestFactory, TestCase, override_settings
+from django.test import Client, RequestFactory, TestCase, override_settings
+from django.urls import reverse
+from apps.auth.models import UserProfile
 
 from apps.clubs.models import Club
 from .models import Manager
@@ -50,21 +53,26 @@ class ManagerModelTest(TestCase):
 
     # Delete
     def test_manager_deletion(self):
-        manager_id = self.manager.id;
-        self.assertIsNotNone(manager_id)
+        self.assertIsNotNone(self.manager.id)
         self.manager.delete()
         self.assertEqual(Manager.objects.count(), 0)
         with self.assertRaises(Manager.DoesNotExist):
-            Manager.objects.get(id=manager_id)
+            Manager.objects.get(id=self.manager.id)
 
 class ManagerViewsTest(TestCase):
     @override_settings(MEDIA_ROOT=os.path.join('tmp'))
     def setUp(self):
+        """
+        Create a temporary directory for MEDIA_ROOT called /tmp.
+        The uploaded images from the club and manager creation will be saved
+        in /tmp.
+        """
         os.makedirs('tmp', exist_ok=True)
 
-        self.rf = RequestFactory()
-        self.user = User.objects.create(username='testuser',
-                                        password='testpassword')
+        self.client = Client()
+
+        self.admin = User.objects.create_user(username='admin', password='admin123')
+        UserProfile.objects.create(user=self.admin, type='admin')
 
         club_logo_path = 'test_media/test_club_logo.png'
         club_logo_name = 'test_club_logo.png'
@@ -72,7 +80,9 @@ class ManagerViewsTest(TestCase):
             logo = SimpleUploadedFile(club_logo_name,
                                       logo_file.read(),
                                       content_type='image/png')
-            self.club = Club.objects.create(name='Test Club',logo=logo)
+            self.club1 = Club.objects.create(name='Test Club 1', logo=logo)
+            self.club2 = Club.objects.create(name='Test Club 2', logo=logo)
+            self.club3 = Club.objects.create(name='Test Club 3', logo=logo)
 
         manager_image_path = 'test_media/test_manager.png'
         manager_image_name = 'test_manager.png'
@@ -80,19 +90,101 @@ class ManagerViewsTest(TestCase):
             image = SimpleUploadedFile(manager_image_name,
                                        image_file.read(),
                                        content_type='image/png')
-            self.manager = Manager.objects.create(name='Test Manager',
-                                                  nationality='English',
+            self.manager1 = Manager.objects.create(name='Test Manager 1',
+                                                  nationality='England',
                                                   dob='1970-01-01',
-                                                  club=self.club,
+                                                  club=self.club1,
                                                   image=image)
+            self.manager2 = Manager.objects.create(name='Test Manager 2',
+                                                  nationality='Spain',
+                                                  dob='1980-02-02',
+                                                  club=self.club2,
+                                                  image=image)
+
+        # Login with admin priviledges
+        self.client.login(username='admin', password='admin123')
+
     @override_settings(MEDIA_ROOT=os.path.join('tmp'))
     def tearDown(self):
+        """
+        Delete the /tmp directory after running all tests.
+        """
         shutil.rmtree(settings.MEDIA_ROOT)
 
     def test_index_view(self):
-        get_request = self.rf.get('/managers/')
-        get_request.user = self.user
-        get_response = index(get_request)
+        url = reverse('managers:index')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'managers/index.html')
+        self.assertContains(response, self.manager1.name)
 
-        self.assertEqual(get_response.status_code, 200)
+    @override_settings(MEDIA_ROOT=os.path.join('tmp'))
+    def test_add_view(self):
+        url = reverse('managers:add')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
 
+        with open('test_media/test_manager.png', 'rb') as image_file:
+            image = SimpleUploadedFile(image_file.name,
+                                       image_file.read(),
+                                       content_type='image/png')
+
+            data = {
+                'name': 'New Manager',
+                'nationality': 'England',
+                'dob': '1978-10-10',
+                'club': self.club3.id,
+                'image': image
+            }
+
+            response = self.client.post(url, data, follow=True)
+            print(response.context['form'].errors)
+            self.assertEqual(response.status_code, 200)
+            self.assertTemplateUsed(response, 'managers/add.html')
+            self.assertTrue(Manager.objects.filter(name='New Manager').exists())
+
+    def test_detail_view(self):
+        url = reverse('managers:view', args=[self.manager1.id])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'managers/view.html')
+        self.assertEqual(response.context['user'], self.admin)
+        self.assertEqual(response.context['manager'], self.manager1)
+
+    @override_settings(MEDIA_ROOT=os.path.join('tmp'))
+    def test_edit_view(self):
+        url = reverse('managers:edit', args=[self.manager1.id])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'managers/add.html')
+
+        with open('test_media/test_manager.png', 'rb') as image_file:
+            image = SimpleUploadedFile(image_file.name,
+                                       image_file.read(),
+                                       content_type='image/png')
+            data = {
+                'name': 'Updated Manager',
+                'nationality': self.manager1.nationality,
+                'dob': self.manager1.dob,
+                'club': self.manager1.club.id,
+                'image': image
+            }
+
+            response = self.client.post(url, data, follow=True)
+            self.assertEqual(response.status_code, 200)
+            self.manager1.refresh_from_db()
+            self.assertEqual(self.manager1.name, 'Updated Manager')
+
+    def test_delete_view(self):
+        url = reverse('managers:delete', args=[self.manager1.id])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302) # Expecting a redirect
+        self.assertFalse(Manager.objects.filter(id=self.manager1.id).exists())
+
+    def test_search_view(self):
+        url = reverse('managers:search')
+        response = self.client.get(url, {'manager_name': "Test Manager 1"})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'managers/search.html')
+        self.assertIn(self.manager1, response.context['found_managers'])
+        self.assertNotIn(self.manager2, response.context['found_managers'])
