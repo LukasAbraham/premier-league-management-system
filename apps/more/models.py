727a60fc8316990d534ff2abb60f97a6522c6504
Tran Xuan Thanh <thanhthcsab@gmail.com>
2023-12-12 09:20:44 +0700

    add manager age constraints for regulation

--- apps/more/models.py
+++ apps/more/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
 class Regulation(models.Model):
-    min_age = models.PositiveIntegerField(default=16)
-    max_age = models.PositiveIntegerField(default=40)
+    player_min_age = models.PositiveIntegerField(default=16)
+    player_max_age = models.PositiveIntegerField(default=40)
+    manager_min_age = models.PositiveIntegerField(default=30)
+    manager_max_age = models.PositiveIntegerField(default=80)
     min_players = models.PositiveIntegerField(default=15)
     max_players = models.PositiveIntegerField(default=40)
     max_foreign_players = models.PositiveIntegerField(default=10)
