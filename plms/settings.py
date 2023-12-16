5ce451fdfa33a2230c0ff8410fed1ab70dd0e617
Tran Xuan Thanh <thanhthcsab@gmail.com>
2023-12-16 09:16:46 +0700

    create a separate db for automation testing

--- plms/settings.py
+++ plms/settings.py
@@ -87,7 +87,14 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    },
+    'test': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'test_db.sqlite3',
+        'TEST': {
+            'MIRROR': 'default',
+        },
+    },
 }
 
 
